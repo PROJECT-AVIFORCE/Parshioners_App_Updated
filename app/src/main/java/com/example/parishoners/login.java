e18fcfde88f1bf57e10da72ff327b0ae57af9529
ashgeo <robotman980@gmail.com>
2023-03-12 16:23:15 +0530

    updated 12/03/23

--- app/src/main/java/com/example/parishoners/login.java
+++ app/src/main/java/com/example/parishoners/login.java
@@ -13,7 +13,6 @@ public class login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
         Button submit=findViewById(R.id.submit);
 
