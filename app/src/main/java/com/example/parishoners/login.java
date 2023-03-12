f559e536a87e671007178e82ffb8964d00c0fa45
vipul <ipulfadte43@gmail.com>
2023-03-12 16:50:23 +0530

    BACKEND UPDATE 21/02/23

--- app/src/main/java/com/example/parishoners/login.java
+++ app/src/main/java/com/example/parishoners/login.java
@@ -7,22 +7,40 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class login extends AppCompatActivity {
+
+    EditText Username;
+    EditText Password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Button submit=findViewById(R.id.submit);
 
+
+        Username = findViewById(R.id.user);
+        Password = findViewById(R.id.pass);
+        Button submit=findViewById(R.id.submit);
 
 
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i=new Intent(login.this, Dashboard.class);
-                startActivity(i);
+
+                String username = Username.getText().toString();
+                String password = Password.getText().toString();
+
+                if (username.equals("admin") && password.equals("11111")) {
+                    Toast.makeText(login.this, "Login successful", Toast.LENGTH_SHORT).show();
+                    Intent i=new Intent(login.this, Dashboard.class);
+                    startActivity(i);
+
+                } else {
+                    Toast.makeText(login.this, "Invalid username or password", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
